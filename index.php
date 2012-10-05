e5c3bef6126c36ca62e47dc1d797b37906dcf067
Robert Weaver <rjweaver@me.com>
2012-10-04 17:24:32 -0700

    Menus display in css3 and Html 5 added background img

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <title>Rob Weaver</title>
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <link href="css/default.css" rel="stylesheet" type="text/css" />     
+        
+        <script type="text/javascript" src="js/anibg.js"></script>
+		<script type="text/javascript">
+                    
+			function load(){ 
+                            new AniBg("backgroundCanvas, wow_macro2.png").start();
+			};
+		</script>
+    
+    </head>
+    
+    <!--<body onLoad="load()">
+        <!--<canvas width="200" height="200" id="background1" style="border:1px solid black;">blahaaa</canvas> -->
+
+    <body>
+
+ <?php
+         
+        ?>
+       <!-- 
+         <div id="backgroundHolder">
+	 	 <canvas id="backgroundCanvas" width="800" height="600"></canvas>
+	 </div>
+        
+        
+         <div id="realbody"> 
+-->
+        
+        <div id="wrapper">
+            
+            <div id="sidemenu">
+                
+                <hgroup>
+                
+                    <a href="#"><h1>Robert Weaver</h1></a>
+                    <a href="#"><h2>Web-Designs</h2></a>
+                
+                </hgroup>
+                
+                <div id="contact">
+                    
+                    <h5 id="contactButton">Contact</h5>
+                    
+                    <ul>
+                        <li><a href=""><h4>Email</h4></a></li>
+                        <li><a href=""><h4>Phone</h4></a></li>
+                        
+                    </ul>
+                </div>
+                
+            </div> <!-- end sidemenu -->
+            
+            <!-- MAIN CONTENT OF PAGE -->
+            
+           <!-- <div id="line"></div> -->
+            
+            <div id="content">
+                
+                <div id="line"></div>
+                
+                <div id="main">
+                    
+                    <ul>
+                        <li><div id="picbox"></div></li>
+                        <li><div id="picbox"></div></li>
+                        <li><div id="picbox"></div></li>
+                        <li><div id="picbox"></div></li>
+                        <li><div id="picbox"></div></li>
+                        <li><div id="picbox"></div></li>
+                    </ul>
+                    
+                </div> <!-- end Main -->
+                
+                <div id="line2"></div>
+                
+                <section>
+                    <h3>- Web Design -</h3>
+                    <p>Simple, clean, and stylish sites for personal or business use.</p>
+                    
+                </section>
+                
+                <section>
+                    <h3>- Implementation -</h3>
+                    <p>HTML5 and CSS3 allow a more dynamic site without more plug-ins.</p>
+                </section>
+                
+            </div>  <!-- end Content -->
+            
+        </div> <!-- end Wrapper -->
+        
+         </div>   <!-- end real body ******* --> 
+        
+        
+        <!-- The Text Heading for page sections, may add animation
+             or move back inside the wrapper at a later time -->
+        
+        <div id="portfolioBox"><h2>Portfolio</h2></div>
+        <div id="serviceBox"><h2>Services</h2></div>
+        
+        
+        
+        
+    </body>
+</html>
